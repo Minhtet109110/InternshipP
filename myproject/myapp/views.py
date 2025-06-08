a93455c5bc173c7888c0e794e5e74afaf624b516
Minhtet109110 <thuraminhtet67@gmail.com>
2025-06-08 15:44:27 -0700

    userprofile

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import UserForm, UserProfileForm
 # Create your views here.
 
 def homepage(request):
     return render(request,'base.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST, request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            return redirect('login')
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'register.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
+def register(request):
+ if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            return redirect('register')
+ else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+ return render(request, 'register.html', {'user_form': user_form, 'profile_form': profile_form})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp.views import *
+from myapp.models import *
+from myapp import views
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,6 +26,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage,name='homepage'),
+    path('project_list/',project_list,name='project_list'),
+    path('register/',views.register,name='register')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
